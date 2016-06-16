712a6fa647736f35f4f552f252b126fccbf9ed84
Andrew Yip <yipcma@gmail.com>
2016-06-16 21:46:51 +0800

    unit test and implement next on management

--- test/components/Results_spec.jsx
+++ test/components/Results_spec.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
+import ReactDOM from 'react-dom'
 import {
   renderIntoDocument,
-  scryRenderedDOMComponentsWithClass
+  scryRenderedDOMComponentsWithClass,
+  Simulate
 } from 'react-addons-test-utils'
 import {List, Map} from 'immutable'
 import Results from '../../src/components/Results'
 import {expect} from 'chai'
 
-describe('Results', () => [
+describe('Results', () => {
 
   it('renders entries with vote counts or zero', () => {
     const pair = List.of('Trainspotting', '28 Days Later')
@@ -24,4 +26,18 @@ describe('Results', () => [
     expect(days).to.contain('28 Days Later')
     expect(days).to.contain('0')
   })
-])
+
+  it('invokes the next callback when next button is clicked', () => {
+    let nextInvoked = false
+    const next = () => nextInvoked = true
+
+    const pair = List.of('Trainspotting', '28 Days Later')
+    const component = renderIntoDocument(
+      <Results pair={pair} tally={Map()} next={next} />
+    )
+    Simulate.click(ReactDOM.findDOMNode(component.refs.next))
+
+    expect(nextInvoked).to.equal(true)
+  })
+
+})
